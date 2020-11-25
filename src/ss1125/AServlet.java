f36ea035f449bc138bfc3c60c2889b522e8785a4
huaibbfjb <1254961614@qq.com>
2020-11-25 10:50:13 +0800

    commit 20201125

---
+++ src/ss1125/AServlet.java
@@ -0,0 +1,12 @@
+package ss1125;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * 作者：LiuYunTao
+ * 日期: 10:35 2020/11/25
+ * 描述：
+ */
+public class AServlet extends HttpServlet {
+
+}
--- src/ss1125/CreateCookieServlet02.java
+++ src/ss1125/CreateCookieServlet02.java
@@ -13,14 +13,14 @@ import java.io.IOException;
  * 日期: 9:28 2020/11/25
  * 描述：
  */
-@WebServlet("/CreateCookieServlet01")
-public class CreateCookieServlet01 extends HttpServlet {
+@WebServlet("/CreateCookieServlet02")
+public class CreateCookieServlet02 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         //解决向网页输出消息(响应)中文乱码
         resp.setContentType("text/html; charset=UTF-8");
         //1.创建cookie对象
-        Cookie cookie=new Cookie("key1","value1");
+        Cookie cookie=new Cookie("key2","value2");
         //2.通知客户端保存cookie
         resp.addCookie(cookie);
         resp.getWriter().write("cookie创建成功！"+cookie.getName()+" "+cookie.getValue());
@@ -31,7 +31,7 @@ public class CreateCookieServlet01 extends HttpServlet {
         //解决向网页输出消息(响应)中文乱码
         resp.setContentType("text/html; charset=UTF-8");
         //1.创建cookie对象
-        Cookie cookie=new Cookie("key1","value1");
+        Cookie cookie=new Cookie("key2","value2");
         //2.通知客户端保存cookie
         resp.addCookie(cookie);
         resp.getWriter().write("cookie创建成功！"+cookie.getName()+" "+cookie.getValue());
